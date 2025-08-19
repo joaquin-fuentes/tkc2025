ed02cc09d5dfbaa4eb1a4d1e507baf386ad6ebf5
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-08-19 11:25:45 -0300

    modifique numero de celulare

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,5 @@
-export const WHATSAPP_NUMBER = "543816097754";
+export const WHATSAPP_NUMBER = "543815195817";
 
 export const EMAIL = "info@tucumankayakclub.com";
 
-export const WHATSAPP_NUMBER_VIEW = "+54 381 609-7754";
-
+export const WHATSAPP_NUMBER_VIEW = "+54 381 519-5817";
